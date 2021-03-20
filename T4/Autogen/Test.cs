58153450edf00903b0eefb2ea2b45faa8b78e3c4
AntonC9018 <anton.curm@gmail.com>
2021-03-20 15:24:29 +0200

    T4 test setup

---
+++ T4/Autogen/Test.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello");
+        }
+    }
+}
