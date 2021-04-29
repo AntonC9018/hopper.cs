e4a46011c47e8cd4d9bf1039562b4763a038acfe
AntonC9018 <anton.curm@gmail.com>
2021-04-29 22:59:46 +0300

    go

---
+++ Mine/Main.cs
@@ -0,0 +1,15 @@
+using Hopper.Core;
+using Hopper.Core.Components.Basic;
+using Hopper.Core.Mods;
+
+namespace Hopper.Mine
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello");
+            Hopper.Core.Main.Init();
+        }
+    }
+}
