6bed84a0603d0f1f782ab8f243d2df1adb36f286
AntonC9018 <anton.curm@gmail.com>
2021-05-24 20:57:39 +0300

    make use of early returns

--- Core/Acting/Movs/Basic.cs
+++ Core/Acting/Movs/Basic.cs
@@ -8,52 +8,56 @@ namespace Hopper.Core.ActingNS
     {
         public static IEnumerable<IntVector2> Basic(Transform transform)
         {
-            if (transform.TryGetClosestPlayerTransform(out var playerTransform))
+            if (!transform.TryGetClosestPlayerTransform(out var playerTransform))
             {
-                var orientation = transform.orientation;
-                var diff = playerTransform.position - transform.position;
-                var diff_ones = diff.Sign();
-                var diff_x = new IntVector2(diff_ones.x, 0);
-                var diff_y = new IntVector2(0, diff_ones.y);
-                
-                // The difference is not diagonal
-                if      (diff_ones.x == 0) yield return diff_y;
-                else if (diff_ones.y == 0) yield return diff_x;
+                yield break;
+            }
 
-                // the difference is diagonal
+            var orientation = transform.orientation;
+            var diff = playerTransform.position - transform.position;
+            var diff_ones = diff.Sign();
+            var diff_x = new IntVector2(diff_ones.x, 0);
+            var diff_y = new IntVector2(0, diff_ones.y);
+            
+            // The difference is not diagonal
+            if      (diff_ones.x == 0) yield return diff_y;
+            else if (diff_ones.y == 0) yield return diff_x;
+
+            // the difference is diagonal
+            else
+            {
+                int dot_x = diff_x.Dot(orientation);
+                int dot_y = diff_y.Dot(orientation);
+
+                if (dot_x >= dot_y)
+                {
+                    yield return diff_x;
+                    yield return diff_y;
+                } 
                 else
                 {
-                    int dot_x = diff_x.Dot(orientation);
-                    int dot_y = diff_y.Dot(orientation);
-
-                    if (dot_x >= dot_y)
-                    {
-                        yield return diff_x;
-                        yield return diff_y;
-                    } 
-                    else
-                    {
-                        yield return diff_y;
-                        yield return diff_x;
-                    }
+                    yield return diff_y;
+                    yield return diff_x;
                 }
             }
         }
 
         public static IEnumerable<IntVector2> Adjacent(Transform transform)
         {
-            if (transform.TryGetClosestPlayerTransform(out var playerTransform))
+            if (!transform.TryGetClosestPlayerTransform(out var playerTransform))
             {
-                var difference = playerTransform.position - transform.position;
-                var direction  = difference.Sign();
-
-                if (direction.x != 0 && direction.y != 0)
-                    yield return direction;
-                if (direction.x != 0)
-                    yield return new IntVector2(direction.x, 0);
-                if (direction.y != 0)
-                    yield return new IntVector2(0, direction.y);
+                yield break;
             }
+
+            var difference = playerTransform.position - transform.position;
+            var direction  = difference.Sign();
+
+            if (direction.x != 0 && direction.y != 0)
+                yield return direction;
+            if (direction.x != 0)
+                yield return new IntVector2(direction.x, 0);
+            if (direction.y != 0)
+                yield return new IntVector2(0, direction.y);
         }
 
         public static IEnumerable<IntVector2> Straight(Transform transform)
@@ -65,25 +69,27 @@ namespace Hopper.Core.ActingNS
 
         public static IEnumerable<IntVector2> Diagonal(Transform transform)
         {
-            if (transform.TryGetClosestPlayerTransform(out var playerTransform))
+            if (!transform.TryGetClosestPlayerTransform(out var playerTransform))
             {
-                var diff = (playerTransform.position - transform.position).Sign();
-                var orientation = transform.orientation == IntVector2.Zero ? diff : transform.orientation;
+                yield break;
+            }
 
-                if (diff.x == 0)
-                {
-                    yield return new IntVector2(orientation.x, diff.y);
-                    yield return new IntVector2(-orientation.x, diff.y);
-                }
-                else if (diff.y == 0)
-                {
-                    yield return new IntVector2(diff.x, orientation.y);
-                    yield return new IntVector2(diff.x, -orientation.y);
-                }
-                else
-                {
-                    yield return diff;
-                }
+            var diff = (playerTransform.position - transform.position).Sign();
+            var orientation = transform.orientation == IntVector2.Zero ? diff : transform.orientation;
+
+            if (diff.x == 0)
+            {
+                yield return new IntVector2(orientation.x, diff.y);
+                yield return new IntVector2(-orientation.x, diff.y);
+            }
+            else if (diff.y == 0)
+            {
+                yield return new IntVector2(diff.x, orientation.y);
+                yield return new IntVector2(diff.x, -orientation.y);
+            }
+            else
+            {
+                yield return diff;
             }
         }
     }
