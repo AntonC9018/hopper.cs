fa0555faa9aeac2620169d742ef74bb54ecb0589
AntonC9018 <anton.curm@gmail.com>
2021-05-29 23:40:22 +0300

    fix annoying issue

--- meta.bat
+++ meta.bat
@@ -1,4 +1,9 @@
 @echo off
+
+cd Tests
+call dotnet restore
+cd ..
+
 cd Meta
 call dotnet run Hopper.Core;../Core/Hopper.Core.csproj Hopper.TestContent;../TestContent/Hopper.TestContent.csproj
 cd ..
